fb51227a64f8f6e186ee8f8ff8664009c9dc6d0d
VitoSatria <19vitosatria@gmail.com>
2026-02-13 16:02:21 +0700

    tambahdanrata

--- src/tambahdanrata.java
+++ src/tambahdanrata.java
@@ -3,7 +3,7 @@ public class tambahdanrata {
         int[] angka = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int total = 0;
         for (int i = 0; i < angka.length; i++) {
-            total += angka[i]; // Sama dengan: total = total + angka[i]
+            total += angka[i];
         }
         System.out.println("Tambah : " + total);
     }
